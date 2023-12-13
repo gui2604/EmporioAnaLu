e8125cbe1e010b5e19847f0a2bd6d9c1d3653a81
gui2604 <guilherme_bsantos@yahoo.com.br>
2023-12-13 13:13:34 -0300

    convertendo para php pt1

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php require("conectionBD.php");?>
+<?php
+$query_rs_produto = "SELECT * FROM tb_produtos INNER JOIN tb_categorias ON tb_produtos.idCategoria = tb_categorias.idCategoria WHERE tb_produtos.ativo = 1 AND tb_produtos.home = 1 ORDER BY tb_produtos.idProduto DESC";
+$rs_produto = mysqli_query( $conn_bd_emporio, $query_rs_produto )or die( mysqli_error( $conn_bd_emporio ) );
+$totalRow_rs_produto = mysqli_num_rows( $rs_produto );
+$row_rs_produto = mysqli_fetch_assoc( $rs_produto );
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
