5d209e4b2b95bf4f7002306f5a92e0003957f85a
GUILHERME BARRETO SANTOS <guilherme.bsantos26@senacsp.edu.br>
2024-01-22 09:41:54 -0300

    separando a galeria em galeria.php

--- index.php
+++ index.php
@@ -22,46 +22,7 @@ $row_rs_produto = mysqli_fetch_assoc($rs_produto);
     <?php include("header.php"); ?>
 
     <main class="container-fluid" style="text-align:center;">
-        <!-- Start wowslider BODY section -->
-        <section>
-            <div id="wowslider-container1">
-                <div class="ws_images slideshow_imagens">
-                    <ul>
-                        <li class="slideshow_item"><img src="./assets/galeria/data1/images/vinho.jpg"
-                                style="border-radius:27px;" alt="" title="" id="wows1_0"></li>
-                        <li class="slideshow_item"><img src="./assets/galeria/data1/images/goiabada_lata.png"
-                                style="border-radius:27px;" alt="" title="" id="wows1_1"></li>
-                        <li class="slideshow_item"><img src="./assets/galeria/data1/images/queijo_napolitano.png"
-                                style="border-radius:27px;" alt="" title="" id="wows1_2"></li>
-                        <li class="slideshow_item"><img src="./assets/galeria/data1/images/queijoParmesao_450x600.jpeg"
-                                style="border-radius:27px;" alt="" title="" id="wows1_3"></li>
-                        <li class="slideshow_item"><img src="./assets/galeria/data1/images/cocadaCremosa_450x600.jpg"
-                                style="border-radius:27px;" alt="" title="" id="wows1_4"></li>
-                        <li class="slideshow_item"><img src="./assets/galeria/data1/images/meiaCura_450x600.jpeg"
-                                style="border-radius:27px;" alt="" title="" id="wows1_5"></li>
-                    </ul>
-                </div>
-                <div class="ws_bullets">
-                    <div>
-                        <a href="#" title=""><span><img src="./assets/galeria/data1/images/vinho.jpg" alt=""></span></a>
-                        <a href="#" title=""><span><img src="./assets/galeria/data1/images/goiabada_lata.png"
-                                    alt=""></span></a>
-                        <a href="#" title=""><span><img src="./assets/galeria/data1/images/bananada.jpg"
-                                    alt=""></span></a>
-                        <a href="#" title=""><span><img src="./assets/galeria/data1/images/queijoParmesao_450x600.jpeg"
-                                    alt=""></span></a>
-                        <a href="#" title=""><span><img src="./assets/galeria/data1/images/cocadaCremosa_450x600.jpg"
-                                    alt=""></span></a>
-                        <a href="#" title=""><span><img src="./assets/galeria/data1/images/meiaCura_450x600.jpeg"
-                                    alt=""></span></a>
-                    </div>
-                </div>
-                <div class="ws_script"><a href="#"></a></div>
-                <div class="ws_shadow"></div>
-            </div>
-        </section>
-        <!-- End wowslider BODY section -->
-
+        <?php include("galeria.php"); ?>
         <?php include("lowMain.php"); ?>
     </main>
 
