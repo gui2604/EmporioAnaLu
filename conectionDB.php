2767d212ae25ab376b195eed828bdd17ec1a0027
gui2604 <guilherme_bsantos@yahoo.com.br>
2024-02-23 10:19:10 -0300

    corrigindo utf8

--- conectionDB.php
+++ conectionDB.php
@@ -3,5 +3,13 @@ $hostname_conn_bd_emporio = "localhost";
 $database_conn_bd_emporio = "bd_emporio";
 $username_conn_bd_emporio = "root";
 $password_conn_bd_emporio = "";
+//Conexão com servidor LOCAWEB
+// $hostname_conn_bd_ctec = "robb0463.publiccloud.com.br:3306";
+// $database_conn_bd_ctec = "ctsdigital2011_bdctec";
+// $username_conn_bd_ctec = "ctsdi_ti38";
+// $password_conn_bd_ctec = "Ti382024!!";
+//Criando a conexão usando as variáveis
 $conn_bd_emporio = mysqli_connect($hostname_conn_bd_emporio, $username_conn_bd_emporio, $password_conn_bd_emporio, $database_conn_bd_emporio) or trigger_error(mysqli_connect_errno(), E_USER_ERROR);
+
+$mysqli_char_set($conn_bd_emporio, 'utf-8');
 ?>
