43ab67e97a43e3885673719000d6ff72533d518c
gui2604 <guilherme_bsantos@yahoo.com.br>
2024-02-26 16:20:12 -0300

    ajustes em telas extragrandes

--- conectionDB.php
+++ conectionDB.php
@@ -1,21 +1,21 @@
 <?php 
-/*
+
 $hostname_conn_bd_emporio = "localhost";
 $database_conn_bd_emporio = "bd_emporio";
 $username_conn_bd_emporio = "root";
 $password_conn_bd_emporio = "";
-*/
 
 
 
 
+/*
 //Conexão com servidor LOCAWEB
 $hostname_conn_bd_emporio = "robb0463.publiccloud.com.br:3306";
 $database_conn_bd_emporio  = "ctsdigital2011_bdemporio";
 $username_conn_bd_emporio  = "ctsdi_analu";
 $password_conn_bd_emporio  = "Doorsoftware22@!";
 //Criando a conexão usando as variáveis
-
+*/
 
 
 
--- galeria.php
+++ galeria.php
@@ -2,7 +2,7 @@
 <section>
     <!-- <iframe class="embed-responsive-item" src="./assets/galeria/wowslider.html" -->
     <iframe src="./assets/galeria/wowslider.html" class="galeria-iframe"
-        style="width:1920px;max-width:100%;display:block;min-width:48rem;"
+        style="width:100vw;display:block;min-width:48rem;"
         marginheight="0" marginwidth="0" frameborder="0" scrolling="no">
     </iframe>
 </section>
