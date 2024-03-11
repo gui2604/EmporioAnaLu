e8e444ddc74e776ff025d0dd0c9970796e2d93be
gui2604 <guilherme_bsantos@yahoo.com.br>
2024-03-11 09:07:32 -0300

    arrumando SEO pt2

--- conectionDB.php
+++ conectionDB.php
@@ -1,18 +1,18 @@
 <?php 
-
+/*
 $hostname_conn_bd_emporio = "localhost";
 $database_conn_bd_emporio = "bd_emporio";
 $username_conn_bd_emporio = "root";
 $password_conn_bd_emporio = "";
+*/
 
-/*
 //Conexão com servidor LOCAWEB
 $hostname_conn_bd_emporio = "robb0463.publiccloud.com.br:3306";
 $database_conn_bd_emporio  = "ctsdigital2011_bdemporio";
 $username_conn_bd_emporio  = "ctsdi_analu";
 $password_conn_bd_emporio  = "Doorsoftware22@!";
 //Criando a conexão usando as variáveis
-*/
+
 $conn_bd_emporio = mysqli_connect($hostname_conn_bd_emporio, $username_conn_bd_emporio, $password_conn_bd_emporio, $database_conn_bd_emporio) or trigger_error(mysqli_connect_errno(), E_USER_ERROR);
 mysqli_set_charset($conn_bd_emporio, 'utf8');
 
